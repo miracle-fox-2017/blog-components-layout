05491fc00a4192b6f85dba71a77822e13699bdc6
azhariemuhammad <azharie_muhammad@live.com>
2017-11-28 14:52:10 +0700

    Read docs from Db

--- miracle-blog/src/App.vue
+++ miracle-blog/src/App.vue
@@ -1,7 +1,39 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/flatly/bootstrap.min.css">
+    <div>
+      <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation" style="">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarColor01">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Features</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Pricing</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">About</a>
+            </li>
+          </ul>
+          <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="text" placeholder="Search">
+            <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+          </form>
+        </div>
+      </nav>
+      <header>
+        <h1>Blog Post</h1>
+      </header>
+      <router-view/>
+    </div>
   </div>
 </template>
 
---
+++ miracle-blog/src/components/BlogPost.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <main>
+      <aside class="sidebar">
+        <div v-for="post in posts">
+          {{ post.title }}
+        </div>
+      </aside>
+      <div class="content">
+      </div>
+    </main>
+  </div>
+</template>
+
+
+<script>
+  import axios from 'axios'
+
+  export default {
+    name: 'BlogPost',
+    data () {
+      return {
+        posts: [],
+        errors: []
+      }
+    },
+    created: function () {
+      axios.get(`http://localhost:4000/api/blog`)
+      .then(response => {
+        console.log(response)
+        this.posts = response.data
+      })
+      .catch(e => {
+        this.errors.push(e)
+      })
+    }
+  }
+</script>
